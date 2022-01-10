e036aeac349f2f4155c0be5f067c3a2ada4ca291
Nima Pourmohamadi <nimaprmdi@gmail.com>
2022-01-10 10:23:27 +0330

    Improve Categories design styles

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -11,7 +11,7 @@ if ( ! function_exists( 'brilliance_posted_on' ) ) :
 	/**
 	 * Prints HTML with meta information for the current post-date/time.
 	 */
-	function brilliance_posted_on(  $brilliance_has_modified_time = false ) {
+	function brilliance_posted_on(  $brilliance_has_modified_time = false , $brilliance_date_class = "" ) {
 		$time_string = '<time class="c-post__entry-date published updated" datetime="%1$s">%2$s</time>';
 		if ( $brilliance_has_modified_time ) {
 			$time_string = '<time class="c-post__entry-date published" datetime="%1$s">%2$s</time><time class="c-post__entry-date__updated" datetime="%3$s">%4$s</time>';
@@ -28,7 +28,7 @@ if ( ! function_exists( 'brilliance_posted_on' ) ) :
 		$posted_on = sprintf(
 			/* translators: %s: post date. */
 			esc_html_x( '%s', 'post date', 'brilliance' ),
-			'<a class="c-post__date" href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+			'<a class="c-post__date '.esc_attr( $brilliance_date_class ).'" href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 		);
 
 		echo '<span class="c-post__posted-on">' . $posted_on . '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
@@ -162,7 +162,7 @@ if ( ! function_exists('brilliance_get_thumbnail')) :
 			);
 		}
 		else{
-			echo '<img alt="'.esc_attr__( 'no thumbnail', 'brilliance' ).'" src="' . esc_url(get_template_directory_uri()). '/assets/images/no-thumbnail.png" />';
+			echo '<img class="c-post__thumbnail__image" alt="'.esc_attr__( 'no thumbnail', 'brilliance' ).'" src="' . esc_url(get_template_directory_uri()). '/assets/images/no-thumbnail.png" />';
 		}
 }
 endif;
@@ -172,16 +172,38 @@ if ( ! function_exists( 'brilliance_post_categories' ) ) :
 	/**
 	 * Show post categories
 	 */
-	function brilliance_post_categories() {
+	function brilliance_post_categories( $brilliance_separator = ", " , $brilliance_cat_class = "" ) {
 		$brilliance_categories = get_the_category();
-
+		$brilliance_output = '';
+		
 		echo "<div class='c-category'>";
 			foreach ( $brilliance_categories as $brilliance_category ) {
-				echo sprintf( '<a href="%s" class="c-category__item__link">%s</a>',
+					/** Translator %s 1: category link function. Translator %s 2: class name. Translator %s 3: category name. Translator %s 4: The separator */
+					$brilliance_output .= sprintf( '<a href="%s" class="c-category__item__link %s">%s</a>%s',
 					esc_url( get_category_link( $brilliance_category->term_id ) ),
-					esc_html( $brilliance_category->name ) );
+					esc_html( $brilliance_cat_class ),
+					esc_html( $brilliance_category->name ),
+					esc_html( $brilliance_separator ) );
 			}
+
+			echo  wp_kses_post(trim( $brilliance_output , $brilliance_separator ));
 		echo "</div>";
+	}
+endif;
+
+
+if (! function_exists('castpress_get_category')) :
+	/**
+	  * Return Post category
+	  */
+	function castpress_get_category( $castpress_have_seprator = false ) {
+		($castpress_have_seprator) ? $castpress_have_seprator = "<span class='seprator h5 u-link--secondary'> ".esc_html( " | " )." </span>" : $castpress_have_seprator = "";
+		/* translators: used between list items, there is a space after the comma */
+		$categories_list = get_the_category_list( esc_html__( ', ', 'castpress' ) );
+		if ( $categories_list ) {
+			/* $categories_list list of categories. Rendered from category section that client set in categories.*/
+			echo '<h5 class="c-episode__category u-font--regular">'.  wp_kses_post($categories_list) .'</h5>' . wp_kses_post($castpress_have_seprator) ;// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		}
 	}
 endif;
 
--- template-parts/content.php
+++ template-parts/content.php
@@ -11,19 +11,25 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('c-post js-post-has-masonry'); ?>>
 
     <div class="c-post__thumbnail">
-        <?php brilliance_get_thumbnail(); ?>
+        <a class="c-post__thumbnail__link" href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark">
+            <?php brilliance_get_thumbnail(); ?>
+        </a>
     </div>
 
-    <div class="c-post__entry-meta">
+    <div class="c-post__entry-header">
 
         <?php 
-			the_title( '<h2 class="c-post__entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			the_title( '<h3 class="c-post__entry-title h3--bold"><a class="u-link--secondary" href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
 
-			brilliance_posted_on();
+			
+			echo wp_kses_post( '<div class="c-post__entry-meta">' );
 
-			brilliance_get_seprator();
-
-			brilliance_post_categories();
+				brilliance_posted_on(false , "u-link--tertiary");
+				brilliance_get_seprator();
+				brilliance_post_categories(", " , "u-link--meta");
+				
+			echo wp_kses_post( '</div>' );
+		
 		?>
 
         <?php
