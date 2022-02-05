099f8e49f5f5776fce4ae59854e565608b03c0f2
Nima Pourmohamadi <nimaprmdi@gmail.com>
2022-02-05 11:51:44 +0330

    Fix screenshot

--- footer.php
+++ footer.php
@@ -29,7 +29,7 @@
                 /** Translator %s 1: current year. Translator %s 2: copyright text. */
                 echo sprintf('<h5 class="c-footer__copy-text u-margin-none">%s %s</h5>' , esc_html( date("Y") ) , esc_html__('Brilliance, made by' , 'brilliance') ); 
                 /** Translator %s 1:Vitathems website, Translator %s 2: Vitathemes name text */
-                echo sprintf( '<a class="c-footer__copy-link u-link--nav" href="%s"><p>%s</p></a>' , esc_url('http://vitathemes.com/') , esc_html__( 'VitaThemes', 'brilliance' ) );  
+                echo sprintf( '<a class="c-footer__copy-link u-link--nav" href="%s"><span class="u-link--tertiary" >%s</span></a>' , esc_url('http://vitathemes.com/') , esc_html__( 'VitaThemes', 'brilliance' ) );  
             ?>
         </div>
         <?php brilliance_socials_links(); ?>
