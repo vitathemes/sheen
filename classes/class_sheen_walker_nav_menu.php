3798d6378a85edd9aeccaa2056c78e7b2181a52a
Nima Pourmohamadi <nimaprmdi@gmail.com>
2022-02-14 09:51:12 +0330

    Fix minor issues

--- classes/class_sheen_walker_nav_menu.php
+++ classes/class_sheen_walker_nav_menu.php
@@ -1,9 +1,12 @@
 <?php
 class Sheen_walker_nav_menu extends Walker_Nav_Menu {
-	function start_el( &$output, $item ) {
-
+	function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+		$object      = $item->object;
+		$type        = $item->type;
 		$title       = $item->title;
+		$description = $item->description;
 		$permalink   = $item->url;
+
 		$output .= "<li class='" . implode( " ", $item->classes ) . "'>";
 	
 		//Add SPAN if no Permalink
